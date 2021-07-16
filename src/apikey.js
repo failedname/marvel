e9ec2b4844178ebe3c057ac6b711507b7b8d48fe
javier nuñez <javier@MacBook-Pro-de-javier.local>
2021-07-15 22:35:21 -0500

    https

--- src/apikey.js
+++ src/apikey.js
@@ -1,6 +1,6 @@
 const publicKey = "454001bec2988d441c590d48763ae9a5";
 const secrectkey = "072bbc0375857ccffff1a23ac51d7b7513b3473a";
 const ts = Date.now();
-const url = "http://gateway.marvel.com/v1/public/characters";
+const url = "https://gateway.marvel.com/v1/public/characters";
 
 export { publicKey, secrectkey, ts, url };
