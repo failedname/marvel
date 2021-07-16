14e214a7dfe375bd85dc454c0552000a01158684
javier nuñez <javier@MacBook-Pro-de-javier.local>
2021-07-15 22:23:44 -0500

    hash delete

--- src/apikey.js
+++ src/apikey.js
@@ -1,6 +1,5 @@
 const publicKey = "454001bec2988d441c590d48763ae9a5";
 const secrectkey = "072bbc0375857ccffff1a23ac51d7b7513b3473a";
-const hash = "245c87c903f9d5096846647c7d317949";
 const ts = Date.now();
 const url = "http://gateway.marvel.com/v1/public/characters";
 
